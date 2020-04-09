1f1b10de8d8412d7ba0cf219dfd136212ed823fa
davidteren <drteren@gmail.com>
2020-04-09 16:33:08 +0200

    configure inter font & tailwind ui plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/ui'),
+    ]
 }
